976e4634946617065aacd1ccc15357708c7f80ab
Aarav Chaturvedi <aaravchaturvedi66official@gmail.com>
2026-02-02 08:51:19 +0000

    added ThemeCOntext

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,20 @@ import { Route, Routes } from "react-router-dom"
 import Layout from "./pages/Layout"
 import Dashboard from "./pages/Dashboard"
 import FoodLog from "./pages/FoodLog"
+import Profile from "./pages/Profile"
+import ActivityLog from "./pages/ActivityLog"
 
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path ='/' element ={<Layout />}></Route>
-        <Route index element ={<Dashboard />}></Route>
-        <Route path ="food" element ={<FoodLog/>}></Route>
+        <Route path ='/' element ={<Layout />}>
+          <Route index element ={<Dashboard />} />
+          <Route path ="food" element ={<FoodLog/>} />
+          <Route path ="profile" element ={<Profile/>} />
+          <Route path ="activity" element ={<ActivityLog/>} />
+        </Route>
       </Routes>
     </>
   )
--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom"
 
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div>
+      Layout
+      < Outlet />
+      
+    </div>
   )
 }
 
